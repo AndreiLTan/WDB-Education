856ac7d7d237a790eacdc60f098a9a2900301f0e
Andrei Tan <m.andrei.tan73@gmail.com>
2022-02-18 21:59:56 -0800

    HW2 webdev

---
+++ demoJS.js
@@ -0,0 +1,9 @@
+function printHome(city, state, country) {
+    console.log(`I am from: ${city}, ${state} ${country}!`);
+}
+
+var city = 'Newport beach';
+var state = "California";
+var country = "USA";
+
+printHome(city, state, country);
