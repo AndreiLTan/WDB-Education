249c84b86a4059b1d448ad2f77138207454ce5d6
Andrei Tan <73627326+AndreiLTan@users.noreply.github.com>
2022-02-18 23:26:08 -0800

    Delete demoJS.js
    unneeded

--- demoJS.js
+++
@@ -1,9 +0,0 @@
-function printHome(city, state, country) {
-    console.log(`I am from: ${city}, ${state} ${country}!`);
-}
-
-var city = 'Newport beach';
-var state = "California";
-var country = "USA";
-
-printHome(city, state, country);
